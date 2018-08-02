8c8c980f93a9c18ba38e5b3c2cc6c0d0af41689b
Owen Stowe <ostowe@gmail.com>
2018-08-01 21:05:54 -0400

    Switch to using an object for renderedModules to make it faster

--- src/getCss.js
+++ src/getCss.js
@@ -1,16 +1,16 @@
 // Manifest of rendered module ids
-const renderedModules = [];
+const renderedModules = {};
 
 /**
  * Get CSS content from a module as a string
  */
 function getCss(styles) {
   const stylesToRender = styles.filter((module) => {
-    const [moduleId] = module;
+    const [moduleId, css] = module;
 
     // Check if we've already rendered a module with this ID
-    if (!renderedModules.includes(moduleId)) {
-      renderedModules.push(moduleId);
+    if (!renderedModules[moduleId]) {
+      renderedModules[moduleId] = css;
       return module;
     }
 
